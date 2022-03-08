2d0c717583c420173fbf79a72750eb4aea492716
Maryrosey15 <rose.pajanel@yahoo.com>
2022-03-08 20:19:34 +1300

    practice code for learning

---
+++ PracticeCoding/PracticeCoding.cpp
@@ -0,0 +1,46 @@
+// PracticeCoding.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2;
+    char operation;
+    cout << "**CodeBeauty calculator**"<< endl;
+    cin >> num1 >> operation >> num2;
+
+    switch (operation)
+    {
+    case '-':cout << num1 << operation << num2 << "=" << num1 - num2; break;
+    case '+':cout << num1 << operation << num2 << "=" << num1 + num2; break;
+    case '/':cout << num1 << operation << num2 << "=" << num1 / num2; break;
+    case '*':cout << num1 << operation << num2 << "=" << num1 * num2; break;
+    case '%':
+        bool isNum1Int, isNum2Int;
+        isNum1Int=((int)num1 == num1); //5==5.0
+        isNum2Int = ((int)num2 == num2);
+
+        if (isNum1Int && isNum2Int)
+            cout << num1 << operation << num2 << "=" << (int)num1 % (int)num2;
+        else
+            cout << "Not Valid!";
+        break;
+    default:
+        cout << "Not valid operation!"<< endl;
+
+    }
+    system("pause>0");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
